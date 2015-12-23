63c636833ca89f0c9f1b99be114964b63ef06cac
jacek-zyla <jacek.zyla@schibsted.pl>
2015-12-23 10:13:23 +0100

    ESI count

--- routes/book.js
+++ routes/book.js
@@ -4,26 +4,22 @@ const request = require('good-guy-http')({
   maxRetries: 3,
   json: true
 });
+
 const jp100 = require('jsonpath');
 const ESI = require('nodesi');
 
 module.exports = (req, res, next) => {
   const isbn = req.params.isbn;
 
-  const detailsPromise = request(`https://tspdyqxhrd.localtunnel.me/book?isbn=${isbn}`);
-  const countPromise = request(`https://book-inventory-jm.herokuapp.com/stock/${isbn}`);
-
-  Promise.all([detailsPromise, countPromise])
+  request(`https://book-catalog-proxy.herokuapp.com/book?isbn=${isbn}`)
     .then((data) => {
-      const title = jp100.query(data[0], '$..title');
-      const thumbnail = jp100.query(data[0], '$..thumbnail');
-      const count = jp100.query(data[1], '$..count');
+      const title = jp100.query(data, '$..title');
+      const thumbnail = jp100.query(data, '$..thumbnail');
 
       return new Promise((resolve, reject) => {
         req.app.render('book', {
           title,
           thumbnail,
-          count,
           partials: {
             layout: 'layout'
           }
@@ -35,10 +31,15 @@ module.exports = (req, res, next) => {
     })
     .then(html => {
       const esi = new ESI({
+        cache: false,
         onError: (src, error) => `<!-- GET ${src} resulted with ${error} -->`
       });
 
-      return esi.process(html);
+      return esi.process(html, {
+        headers: {
+          Accept: 'text/html'
+        }
+      });
     })
     .then(html => res.send(html))
     .catch((errs) => {
